a54fa699edcfd842441e7247c5f696259451eeb7
jlsicajan <sicajancoy@outlook.com>
2017-12-21 00:37:27 -0600

    Default panel profile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,14 +18,58 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
+<div class="profile">
+    <div class="image" style="background-image: url({{ asset("images/profile/deathstroke.jpg") }})">asdas</div>
+    <div class="user-info">
+        Sed aliquam ultrices mauris. Aliquam lobortis. Praesent adipiscing. Phasellus gravida semper nisi. Suspendisse eu ligula.
+
+        Pellentesque auctor neque nec urna. Fusce egestas elit eget lorem. Vestibulum fringilla pede sit amet augue. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Fusce commodo aliquam arcu.
+
+        Cras risus ipsum, faucibus ut, ullamcorper id, varius ac, leo. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Vivamus quis mi. Curabitur blandit mollis lacus. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.
+
+        Ut tincidunt tincidunt erat. Nam eget dui. Mauris turpis nunc, blandit et, volutpat molestie, porta ut, ligula. Ut tincidunt tincidunt erat. Praesent nonummy mi in odio.
+
+        Aliquam eu nunc. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor. Vestibulum volutpat pretium libero. Donec mi odio, faucibus at, scelerisque quis, convallis in, nisi. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor.
+    </div>
+</div>
 @include('cv.head')
 <div class="container">
     <div class="row">
-        <div class="col-md-6">hola</div>
-        <div class="col-md-6">123</div>
+        <div class="col-md-6"></div>
+        <div class="col-md-6"></div>
     </div>
 </div>
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+
+    $(document).ready(function () {
+        resize_update();
+    });
+
+    $(window).on('resize', function () {
+        resize_update();
+    });
+
+    function resize_update() {
+        console.log("Window width => " + $(window).width());
+        if ($(window).width() > 1300) {
+            $('.navbar-default').addClass('give_space');
+            $('.container').addClass('give_space');
+            $('.profile').show();
+            if ($(window).width() > 2000) {
+                $('.navbar-default').removeClass('give_space');
+                $('.navbar-default').addClass('m-l-500');
+            }else{
+                $('.navbar-default').removeClass('m-l-500');
+            }
+        } else {
+            $('.navbar-default').removeClass('give_space');
+            $('.container').removeClass('give_space');
+            $('.navbar-default').removeClass('m-l-500');
+            $('.profile').hide();
+        }
+    }
+</script>
 </body>
 </html>

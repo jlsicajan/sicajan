4c6239c9f055c3b61f22c6a97d3ed822c10a635a
Jose Sicajan <sicajancoy@gmail.com>
2018-03-01 22:52:48 -0600

    Webpack configurations and bootstrap4

--- resources/views/cv/portfolio.blade.php
+++ resources/views/cv/portfolio.blade.php
@@ -6,16 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>JL</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- Styles -->
-    <link rel="stylesheet" type="text/css" href="css/cv/cv.css">
-    <!-- Style for portafolio -->
-    <link rel="stylesheet" type="text/css" href="css/cv/portfolio.css">
+    <link href="/css/app.css" rel="stylesheet">
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
@@ -28,7 +20,6 @@
     @endforeach
     </div>
 </div>
-
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="/js/app.js"></script>
 </body>
 </html>
